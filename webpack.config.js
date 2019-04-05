77af76c333242ac8e43aacbc0ccdea69bbf78275
roman <gordonfreeman2332@gmail.com>
2019-04-05 16:46:23 +0300

    change public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/homework-module3-repository/';
+  const publicPath = '/huskyjam/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
